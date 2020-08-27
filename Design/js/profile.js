b99b881684bb00c1379cb978e7fcf2d58e0ca1c9
mahmoudsaeed99 <36303424+mahmoudsaeed99@users.noreply.github.com>
2020-08-27 20:52:53 +0200

    mmm

--- Design/js/profile.js
+++
@@ -1,8 +0,0 @@
-$("#operation").click(function() {
-    $(".operator").slideToggle(1000);
-    $(".book").hide(1000);
-})
-$("#book").click(function() {
-    $(".book").slideToggle(1000);
-    $(".operator").hide(1000);
-})
